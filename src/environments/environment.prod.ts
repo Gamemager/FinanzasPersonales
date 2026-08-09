4e0e6f67bb035cf71d3e13449eef2c9b5d847de2
Eixon De La Torres <136131912+Gamemager@users.noreply.github.com>
2026-08-08 20:01:44 -0500

    Actualizar environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // Si el backend es un sitio Netlify separado, usa su URL completa:
   // apiUrl: 'https://finanzas-api.netlify.app/api'
-  apiUrl: '/api',
+  apiUrl: 'https://financesbackend.netlify.app/api',
 };
